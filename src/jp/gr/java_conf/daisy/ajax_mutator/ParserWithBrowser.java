74ffad7813eb3881351ec03fa133e82c74bcf7a4
kazuki <k-nishiura@nii.ac.jp>
2012-09-28 17:37:35 +0900

    treat rhino.1.2.js as resource file

--- src/jp/gr/java_conf/daisy/ajax_mutator/ParserWithBrowser.java
+++ src/jp/gr/java_conf/daisy/ajax_mutator/ParserWithBrowser.java
@@ -1,8 +1,11 @@
 package jp.gr.java_conf.daisy.ajax_mutator;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import org.mozilla.javascript.CompilerEnvirons;
 import org.mozilla.javascript.Context;
@@ -12,8 +15,13 @@ import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.tools.shell.Global;
 import org.mozilla.javascript.tools.shell.Main;
 
+/**
+ * JavaScript parser with browser environment (e.g., document object).
+ * 
+ * @author Kazuki Nishiura
+ */
 public class ParserWithBrowser extends Parser {
-	private static String PATH_TO_ENV_JS = "res/env.rhino.1.2.js";
+	private static String PATH_TO_ENV_JS = "/env.rhino.1.2.js";
 	private static Global global = null;
 
 	private ParserWithBrowser(CompilerEnvirons compilerEnvirons) {
@@ -28,14 +36,19 @@ public class ParserWithBrowser extends Parser {
 		}
 		Scriptable scope = context.initStandardObjects(global);
 		context.setOptimizationLevel(-1);
+		URL url = ParserWithBrowser.class.getResource(PATH_TO_ENV_JS);
+		if (url == null)
+			throw new IllegalStateException("Cannot access ENV_JS");
 		try {
-			context.evaluateReader(scope, new FileReader(PATH_TO_ENV_JS),
+			context.evaluateReader(scope, new FileReader(new File(url.toURI())),
 					"env.rhino.1.2", 1, null);
 		} catch (FileNotFoundException e) {
 			throw new IllegalStateException("Environment file '" + PATH_TO_ENV_JS
 					+ "' does not exist.");
 		} catch (IOException e) {
 			throw new IllegalStateException("Instantiating parser failed.");
+		} catch (URISyntaxException e) {
+			throw new IllegalStateException("Cannot access ENV_JS");
 		}
 		CompilerEnvirons compilerEnvirons = new CompilerEnvirons();
 		compilerEnvirons.initFromContext(context);
