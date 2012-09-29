974c44cbc9058716ce08914088821123188e82d7
kazuki <k-nishiura@nii.ac.jp>
2012-09-29 16:06:54 +0900

    implement mutation->save to file->undo in MutationTestConductor

--- src/jp/gr/java_conf/daisy/ajax_mutator/MutationTestConductor.java
+++ src/jp/gr/java_conf/daisy/ajax_mutator/MutationTestConductor.java
@@ -1,47 +1,65 @@
 package jp.gr.java_conf.daisy.ajax_mutator;
 
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jp.gr.java_conf.daisy.ajax_mutator.detector.EventAttacherDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AddEventDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AddEventListenerDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AttachEventDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.mutatable.EventAttachment;
+import jp.gr.java_conf.daisy.ajax_mutator.mutator.EventTargetMutator;
+import jp.gr.java_conf.daisy.ajax_mutator.mutator.Mutator;
 
 import org.mozilla.javascript.ast.AstRoot;
 
 /**
  * Executor to apply mutation testing to target applications.
+ * Note: Currently we assume that mutation target is single JavaScript file.
  * 
  * @author Kazuki Nishiura
  */
 public class MutationTestConductor {
 	private boolean setup;
 	private ParserWithBrowser parser;
-	private MutateVisitor visitor;
+	private String pathToJSFile;
+	private AstRoot astRoot;
+	private Set<EventAttachment> eventAttachments;
+	private List<Mutator> mutators;
 	
 	public MutationTestConductor() {
 		setup = false;
 	}
 	
-	public boolean setup(FileReader fileReader, String targetURL) {
+	/**
+	 * @return if setup is successfully finished.
+	 */
+	public boolean setup(String pathToJSFile, String targetURL) {
+		this.pathToJSFile = pathToJSFile;
 		parser = ParserWithBrowser.getParser();
-		AstRoot ast = null;
 		try {
-			ast = parser.parse(fileReader, targetURL, 1);
+			FileReader fileReader = new FileReader(new File(pathToJSFile));
+			astRoot = parser.parse(fileReader, targetURL, 1);
 		} catch (IOException e) {
 			System.err.println("IOException: cannot parse AST.");
 			return false;
 		}
-		if (ast != null) {
+		if (astRoot != null) {
 			EventAttacherDetector[] attahcerDetectorArray 
-				= {new AddEventListenerDetector(), new AttachEventDetector()};
+				= {new AddEventDetector()};
 			Set<EventAttacherDetector> attacherDetector 
 				= new HashSet<EventAttacherDetector>(Arrays.asList(attahcerDetectorArray));
-			visitor = new MutateVisitor(attacherDetector);
-			ast.visit(visitor);
+			MutateVisitor visitor = new MutateVisitor(attacherDetector);
+			astRoot.visit(visitor);
+			eventAttachments = visitor.getEventAttachments();
+			Mutator[] mutatorsArray = {new EventTargetMutator(eventAttachments)};
+			mutators = Arrays.asList(mutatorsArray);
 			setup = true;
 			return true;
 		} else {
@@ -61,8 +79,29 @@ public class MutationTestConductor {
 	 */
 	public void conduct(TestExecutor testExecutor) {
 		checkIfSetuped();
-		String result = testExecutor.execute();
-		System.out.println(result);
+		for (int i = 0; i < mutators.size(); i++) {
+			Mutator mutator = mutators.get(i);
+			while (!mutator.isFinished()) {
+				mutator.applyMutation();
+				FileWriter writer = null;
+				try {
+					writer = new FileWriter(new File(pathToJSFile));
+					writer.write(astRoot.toSource());
+				} catch (IOException e) {
+					System.err.println("IOException" + e.getMessage());
+				} finally {
+					try {
+						writer.close();
+					} catch (IOException e) {
+						System.err.println("Fail to close source file" + e.getMessage());
+					}
+				}
+				
+				String result = testExecutor.execute();
+				System.out.println(result);
+				mutator.undoMutation();
+			}
+		}
 	}
 	
 	public void conductWithJunit4(Class<?>... classes) {
