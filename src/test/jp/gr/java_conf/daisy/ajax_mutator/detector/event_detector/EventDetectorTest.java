655be588914b5919dba83cff54845d5f7df57483
kazuki <k-nishiura@nii.ac.jp>
2012-09-30 22:31:32 +0900

    added test for AddEventDetector

--- src/test/jp/gr/java_conf/daisy/ajax_mutator/detector/event_detector/EventDetectorTest.java
+++ src/test/jp/gr/java_conf/daisy/ajax_mutator/detector/event_detector/EventDetectorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import jp.gr.java_conf.daisy.ajax_mutator.ParserWithBrowser;
 import jp.gr.java_conf.daisy.ajax_mutator.detector.EventAttacherDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AddEventDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AddEventListenerDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AttachEventDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.mutatable.EventAttachment;
@@ -38,7 +39,17 @@ public class EventDetectorTest {
 
 		assertTrue(detector.detect(stringToFunctionCall(
 				"target.addEventListener('click', func);")) == null);
-		
+	}
+	
+	@Test
+	public void testAddEventDetector() {
+		EventAttacherDetector detector = new AddEventDetector();
+
+		assertTrue(detector.detect(stringToFunctionCall(
+				"addEvent(target, 'click', func);")) != null);
+
+		assertTrue(detector.detect(stringToFunctionCall(
+				"target.addEventListener('click', func);")) == null);
 	}
 	
 	private FunctionCall stringToFunctionCall(String javaScriptSnipet) {
