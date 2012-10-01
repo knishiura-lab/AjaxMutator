f59ed7fbc23bbb5a7eec4669933cc531eac11548
kazuki <k-nishiura@nii.ac.jp>
2012-10-01 13:12:26 +0900

    added test for TimerEventDetector

--- src/test/jp/gr/java_conf/daisy/ajax_mutator/detector/event_detector/EventDetectorTest.java
+++ src/test/jp/gr/java_conf/daisy/ajax_mutator/detector/event_detector/EventDetectorTest.java
@@ -7,13 +7,17 @@ import jp.gr.java_conf.daisy.ajax_mutator.detector.EventAttacherDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AddEventDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AddEventListenerDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AttachEventDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.TimerEventDetector;
 import jp.gr.java_conf.daisy.ajax_mutator.mutatable.EventAttachment;
+import jp.gr.java_conf.daisy.ajax_mutator.mutatable.TimerEventAttachment;
 
 import org.junit.Test;
+import org.mozilla.javascript.ast.AstNode;
 import org.mozilla.javascript.ast.AstRoot;
 import org.mozilla.javascript.ast.ExpressionStatement;
 import org.mozilla.javascript.ast.FunctionCall;
 import org.mozilla.javascript.ast.Name;
+import org.mozilla.javascript.ast.NumberLiteral;
 
 public class EventDetectorTest {
 	@Test
@@ -48,6 +52,25 @@ public class EventDetectorTest {
 		assertTrue(detector.detect(stringToFunctionCall(
 				"addEvent(target, 'click', func);")) != null);
 
+		assertTrue(detector.detect(stringToFunctionCall(
+				"target.addEventListener('click', func);")) == null);
+	}
+	
+	@Test
+	public void testTimerEventDetector() {
+		TimerEventDetector detector = new TimerEventDetector();
+		assertTrue(detector.detect(stringToFunctionCall(
+				"setInterval(func, 1000)")) != null);
+		assertTrue(detector.detect(stringToFunctionCall(
+				"window.setTimeout(func, callAfter)")) != null);
+		assertTrue(detector.detect(stringToFunctionCall(
+				"window.setInterval(func, funcName)")) != null);
+		
+		TimerEventAttachment attachment
+			= detector.detect(stringToFunctionCall("setTimeout(func, 1000)"));
+		NumberLiteral duration = (NumberLiteral) attachment.getDuration();
+		assertEquals(1000.0, duration.getNumber(), 0.0000000000001);
+		
 		assertTrue(detector.detect(stringToFunctionCall(
 				"target.addEventListener('click', func);")) == null);
 	}
