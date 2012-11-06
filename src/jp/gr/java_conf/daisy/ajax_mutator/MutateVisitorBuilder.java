105a95e0eb2f9e560bff3cf0d4e0495487494324
kazuki <k-nishiura@nii.ac.jp>
2012-11-06 17:37:00 +0900

    added MutateVisitorBuilder

---
+++ src/jp/gr/java_conf/daisy/ajax_mutator/MutateVisitorBuilder.java
@@ -0,0 +1,65 @@
+package jp.gr.java_conf.daisy.ajax_mutator;
+
+import java.util.Collections;
+import java.util.Set;
+
+import jp.gr.java_conf.daisy.ajax_mutator.detector.AbstractDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.detector.EventAttacherDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.TimerEventDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.mutatable.AttributeModification;
+import jp.gr.java_conf.daisy.ajax_mutator.mutatable.DOMCreation;
+import jp.gr.java_conf.daisy.ajax_mutator.mutatable.DOMRemoval;
+import jp.gr.java_conf.daisy.ajax_mutator.mutatable.DOMSelection;
+import jp.gr.java_conf.daisy.ajax_mutator.mutatable.Request;
+
+public class MutateVisitorBuilder {
+	private Set<EventAttacherDetector> eventAttacherDetectors
+		= Collections.emptySet();
+	private Set<TimerEventDetector> timerEventDetectors
+		= Collections.emptySet();
+	private Set<? extends AbstractDetector<DOMCreation>> domCreationDetectors
+		= Collections.emptySet();
+	private Set<? extends AbstractDetector<AttributeModification>> 
+		attributeModificationDetectors = Collections.emptySet();
+	private Set<? extends AbstractDetector<DOMRemoval>> domRemovalDetectors
+		= Collections.emptySet();
+	private Set<? extends AbstractDetector<DOMSelection>> domSelectionDetectors
+		= Collections.emptySet();
+	private Set<? extends AbstractDetector<Request>> requestDetectors
+		= Collections.emptySet();
+
+	public MutateVisitor build() {
+		return new MutateVisitor(eventAttacherDetectors, timerEventDetectors, 
+				domCreationDetectors, attributeModificationDetectors, 
+				domRemovalDetectors, domSelectionDetectors, requestDetectors);
+	}
+	
+	public void setEventAttacherDetectors(
+			Set<EventAttacherDetector> eventAttacherDetectors) {
+		this.eventAttacherDetectors = eventAttacherDetectors;
+	}
+	public void setTimerEventDetectors(
+			Set<TimerEventDetector> timerEventDetectors) {
+		this.timerEventDetectors = timerEventDetectors;
+	}
+	public void setDomCreationDetectors(
+			Set<? extends AbstractDetector<DOMCreation>> domCreationDetectors) {
+		this.domCreationDetectors = domCreationDetectors;
+	}
+	public void setAttributeModificationDetectors(
+			Set<? extends AbstractDetector<AttributeModification>> attributeModificationDetectors) {
+		this.attributeModificationDetectors = attributeModificationDetectors;
+	}
+	public void setDomRemovalDetectors(
+			Set<? extends AbstractDetector<DOMRemoval>> domRemovalDetectors) {
+		this.domRemovalDetectors = domRemovalDetectors;
+	}
+	public void setDomSelectionDetectors(
+			Set<? extends AbstractDetector<DOMSelection>> domSelectionDetectors) {
+		this.domSelectionDetectors = domSelectionDetectors;
+	}
+	public void setRequestDetectors(
+			Set<? extends AbstractDetector<Request>> requestDetectors) {
+		this.requestDetectors = requestDetectors;
+	}
+}
