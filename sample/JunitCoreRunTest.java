cd22e4d26da9273e478834deca18eeb5b649f482
kazuki <k-nishiura@nii.ac.jp>
2012-11-12 18:58:02 +0900

    removed Main.java since it's acutally unused, instead adding samle folder to show sample usage

---
+++ sample/JunitCoreRunTest.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import jp.gr.java_conf.daisy.ajax_mutator.MutateVisitor;
+import jp.gr.java_conf.daisy.ajax_mutator.MutationTestConductor;
+import jp.gr.java_conf.daisy.ajax_mutator.detector.EventAttacherDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.detector.event_detector.AddEventDetector;
+import jp.gr.java_conf.daisy.ajax_mutator.mutatable.EventAttachment;
+import jp.gr.java_conf.daisy.ajax_mutator.mutator.EventTargetMutator;
+import jp.gr.java_conf.daisy.ajax_mutator.mutator.EventTypeMutator;
+import jp.gr.java_conf.daisy.ajax_mutator.mutator.Mutator;
+
+import com.google.common.collect.ImmutableSet;
+
+public class JunitCoreRunTest {
+	public static void main(String[] args) {
+		MutationTestConductor conductor = new MutationTestConductor();
+		EventAttacherDetector[] attahcerDetectorArray
+			= {new AddEventDetector()};
+		Set<EventAttacherDetector> attacherDetector
+			= new HashSet<EventAttacherDetector>(Arrays.asList(attahcerDetectorArray));
+		MutateVisitor visitor = new MutateVisitor(attacherDetector, null, null, null, null, null, null);
+
+		conductor.setup("Path_to_AjaxLogin/login_presentation.js", "test_target_URI", visitor);
+
+		Set<EventAttachment> eventAttachments = visitor.getEventAttachments();
+		Set<Mutator> mutators = ImmutableSet.of(
+				(Mutator) new EventTargetMutator(System.out, eventAttachments),
+				(Mutator) new EventTypeMutator(System.out, eventAttachments));
+
+		conductor.conductWithJunit4(mutators, LoginTest.class);
+	}
+}
---
+++ sample/LoginTest.java
@@ -0,0 +1,111 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Test case for AjaxLogin (http://www.jamesdam.com/ajax_login/login.html)
+ *
+ * @author Kazuki Nishiura
+ */
+public class LoginTest {
+	private static WebDriver driver;
+	private static long WAIT_LIMIT_SEC = 1;
+
+	@BeforeClass
+	static public void setUpBrowser() {
+        driver = new FirefoxDriver();
+	}
+
+	@AfterClass
+	static public void terminateBrowser() {
+		driver.close();
+	}
+
+	@Before
+	public void accessURL() {
+        driver.get("Path_to_AjaxLogin/login.html");
+	}
+
+	@Test
+	public void testLoginSuccessAndLogout() {
+		tryLogin("name", "pass");
+		WebElement loginResult = getLoginResult();
+		assertEquals("full.name", loginResult.getText());
+
+		WebElement logoutA = driver.findElement(By.id("login")).findElement(By.tagName("a"));
+		logoutA.click();
+		WebDriverWait wait = new WebDriverWait(driver, WAIT_LIMIT_SEC);
+		WebElement result = null;
+		try {
+			result = wait.until(new ExpectedCondition<WebElement>(){
+				@Override
+				public WebElement apply(WebDriver driver) {
+					WebElement loginDiv = driver.findElement(By.id("login"));
+					WebElement p = loginDiv.findElement(By.tagName("p"));
+					if ("Enter your username and password to log in.".equals(p.getText())) {
+						return p;
+					} else {
+						return null;
+					}
+				}});
+		} catch (Exception e) {
+			System.err.print(e.getClass());
+			fail("Afgter logout text should be set appropriately");
+			return;
+		}
+		assertTrue("After logout text should be set", result != null);
+	}
+
+	@Test
+	public void testLoginInvalidPassword() {
+		tryLogin("name", "passs");
+		WebElement loginResult = getLoginResult();
+		assertEquals("Invalid username and password combination.", loginResult.getText());
+	}
+
+	@Test
+	public void testLoginInvalidUserName() {
+		tryLogin("invalid_name", "pass");
+		WebElement loginResult = getLoginResult();
+		assertEquals("Invalid username and password combination.", loginResult.getText());
+	}
+
+	private void tryLogin(String userName, String password) {
+        WebElement userNameInput = driver.findElement(By.name("username"));
+        userNameInput.sendKeys(userName);
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+
+        // set focus to another element
+        driver.findElement(By.id("comments")).click();
+	}
+
+	private WebElement getLoginResult() {
+		WebElement loginedResult = null;
+		try {
+			loginedResult = (new WebDriverWait(driver, WAIT_LIMIT_SEC))
+				.until(new ExpectedCondition<WebElement>(){
+					@Override
+					public WebElement apply(WebDriver driver) {
+						WebElement loginDiv = driver.findElement(By.id("login"));
+						WebElement p = loginDiv.findElement(By.tagName("p"));
+						return p.findElement(By.tagName("strong"));
+					}});
+		} catch (Exception e) {
+			fail("Timeout!");
+		}
+		return loginedResult;
+	}
+}
