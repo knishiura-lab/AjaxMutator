5406fc7fdbda4036cb77338684377c0c1865c6ca
kazuki <k-nishiura@nii.ac.jp>
2012-11-19 13:42:11 +0900

    show 'which test kill the mutants"

--- src/jp/gr/java_conf/daisy/ajax_mutator/JUnitExecutor.java
+++ src/jp/gr/java_conf/daisy/ajax_mutator/JUnitExecutor.java
@@ -1,7 +1,15 @@
 package jp.gr.java_conf.daisy.ajax_mutator;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 /**
  * TestExecutor for testclasses written in Junit4.
@@ -20,13 +28,36 @@ public class JUnitExecutor implements TestExecutor {
 	public boolean execute() {
 		JUnitCore core = new JUnitCore();
 		Result result = core.run(targetClasses);
+		List<String> testMethods = new ArrayList<String>();
+		for (Class<?> clazz: targetClasses) {
+			for (Method method: clazz.getMethods()) {
+				if (method.isAnnotationPresent(Test.class))
+					testMethods.add(method.getName());
+			}
+		}
+		Map<String, Boolean> testSucceed = new TreeMap<String, Boolean>();
+		for (String methodName: testMethods)
+			testSucceed.put(methodName, true);
+
+		StringBuilder messageBuilder = new StringBuilder();
 		if (result.wasSuccessful()) {
-			executionMessage
-				= "Success! " + result.getRunCount() + " tests ran.";
+			messageBuilder.append("Test succeed (failed to kill mutants), ")
+				.append(result.getRunCount()).append(" tests ran.\n");
+			for (int i = 0; i < testSucceed.size(); i++)
+				messageBuilder.append("x ");
+			messageBuilder.append("x");
+			executionMessage = messageBuilder.toString();
 			return true;
 		} else {
-			executionMessage = result.getFailureCount() + " tests fail within "
-					+ result.getRunCount();
+			messageBuilder.append(result.getFailureCount())
+				.append(" tests failed within ").append(result.getRunCount())
+				.append('\n');
+			for (Failure failure: result.getFailures())
+				testSucceed.put(failure.getDescription().getMethodName(), false);
+			for (Map.Entry<String, Boolean> entry: testSucceed.entrySet())
+				messageBuilder.append(entry.getValue() ? 'x' : 'o').append(' ');
+			messageBuilder.append("o");
+			executionMessage = messageBuilder.toString();
 			return false;
 		}
 	}
